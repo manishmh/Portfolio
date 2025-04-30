4e3c008461dcd53f82fad43d558ef5bb89b4e94c
manishmh <manishmh982@gmail.com>
2025-04-30 12:54:45 +0530

    floating nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import { FloatingNav } from "@/components/FloatingNav";
+import { navItems } from "@/data";
+
 export default function Home() {
   return (
-    <div>Manish</div> 
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <div className="h-[200vh]"></div>
+      </div>
+    </main>
   );
 }

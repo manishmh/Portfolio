ff88a913305a92d073ffe6575c576bfe47f15ed4
manishmh <manishmh982@gmail.com>
2025-05-02 20:24:56 +0530

    removed moving border

--- src/components/WorkExp.tsx
+++ src/components/WorkExp.tsx
@@ -1,8 +1,6 @@
-/* eslint-disable @next/next/no-img-element */
 import { workExperience } from "@/data";
-import React from "react";
-import { Button } from "./ui/MovingBorders";
 import Image from "next/image";
+import React from "react";
 
 const WorkExp = () => {
   return (
@@ -12,30 +10,26 @@ const WorkExp = () => {
       </h1>
       <div className="w-full mt-12 grid lg:grid-cols-2 grid-cols-1 gap-10 ">
         {workExperience.map(({ thumbnail, id, title, desc }) => (
-          // <Button
-          //   key={id}
-          //   borderRadius="1.75rem"
-          //   className="flex-1 text-white border-neutral-200 dark:border-slate-800"
-          //   duration={Math.floor(Math.random() * 10000) + 10000}
-          // >
-            <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2" key={id}>
-              <Image
-                src={thumbnail}
-                alt={thumbnail}
-                width={128} // Assuming 32 * 4 for the largest size (lg)
-                height={128} // Assuming a square image, adjust if not
-                className="lg:w-32 md:w-20 w-16"
-              />
-              <div className="lg:ms-5">
-                <h1 className="text-start text-xl md:text-2xl font-bold">
-                  {title}
-                </h1>
-                <p className="text-start text-white-100 mt-3 font-medium text-[#b5b7d3]">
-                  {desc}
-                </p>
-              </div>
+          <div
+            className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-7 gap-2 bg-[#0C1327] rounded-3xl  border border-[#041c60]"
+            key={id}
+          >
+            <Image
+              src={thumbnail}
+              alt={thumbnail}
+              width={128} // Assuming 32 * 4 for the largest size (lg)
+              height={128} // Assuming a square image, adjust if not
+              className="lg:w-32 md:w-20 w-16"
+            />
+            <div className="lg:ms-5">
+              <h1 className="text-start text-xl md:text-2xl font-bold">
+                {title}
+              </h1>
+              <p className="text-start text-white-100 mt-3 font-medium text-[#b5b7d3]">
+                {desc}
+              </p>
             </div>
-          // </Button>
+          </div>
         ))}
       </div>
     </div>
